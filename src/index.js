b6ad35048cc0478208e81a9b2e818efe39df413c
Edward <e.arechwa@gmail.com>
2021-03-06 23:13:56 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+  if (array && array.length) {
+    return Math.min(...array);
+  } else {
+    return 0;
+  }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array && array.length) {
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array && array.length) {
+    let sum = 0;
+    array.map((item) => (sum += item));
+    return (sum / array.length).toFixed(2);
+  } else {
+    return 0;
+  }
+};
